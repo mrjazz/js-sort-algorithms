878aa56f3513d6312191a9b48211a3df08b5de6f
Aleksey Voronoy <aleksey.voronoy@gmail.com>
2013-03-20 18:46:28 +0200

    radixSort added

--- alexey/main.js
+++ alexey/main.js
@@ -41,3 +41,68 @@ function combSort(a) {
 	}
 	return a
 }
+
+
+/* radix sort */
+
+function radixSortSimple(a) {
+	var k, m = 10, l = a.length
+	var min = 1, max = 12
+	var i = min
+	var temp = {}, output = []
+	do {
+		temp[i++] = 0
+	} while (i <= max)
+	i = 0
+	for (; i < l; i++) {
+		temp[a[i]]++
+	}
+	for (i in temp) {
+		if (temp[i] == 1) {
+			output.push(i)
+		} else {
+			while (temp[i]--) {
+				output.push(i)
+			}
+		}
+	}
+	console.log(output)
+}
+
+function digit_at(n, position) {
+	return n / (Math.pow(10, position)) % 10 | 0
+}
+function radixSort(a) {
+	var i,
+		k = 0, // количество разрядов в самом длинном ключе
+		m = 10, // разрядность данных: количество возможных значений разряда ключа (0..9 -> 10)
+		l = a.length,
+		ix1 = 0
+	for (i in a) {
+		t = (a[i]).toString().length
+		if (k < t) {
+			k = t
+		}
+	}
+
+	for (; ix1 < k; ix1++) {
+		var store = {};
+		for (i = 0; i < l; i++) {
+			var digit = digit_at(a[i], ix1)
+			if (!store[digit]) {
+				store[digit] = []
+			}
+			store[digit].push(a[i])
+		}
+		a = []
+		for (i = 0; i < m; i++) {
+			if (store[i]) {
+				for (x in store[i]) {
+					a.push(store[i][x])
+				}
+			}
+		}
+	}
+	return a
+}
+radixSort(a)
